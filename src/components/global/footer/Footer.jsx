60d739500dda5cd608c8933abf6473f317b7b6de
Richard passos <richard.ac.passos12@gmail.com>
2023-01-29 01:57:22 -0300

    fixing footer date

--- src/components/global/footer/Footer.jsx
+++ src/components/global/footer/Footer.jsx
@@ -4,7 +4,7 @@ import { Footer as FooterStyled } from "./FooterStyle";
 export default function Footer() {
   return (
     <FooterStyled>
-      <p>React Movies @ 2022. Todos os direitos reservados.</p>
+      <p>React Movies @ 2023. Todos os direitos reservados.</p>
     </FooterStyled>
   );
 }
